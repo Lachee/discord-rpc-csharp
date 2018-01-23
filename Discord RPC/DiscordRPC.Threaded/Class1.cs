06382ced61b32d2a667eec289628b76188daea73
Lachee <lachlan.h@hotmail.com>
2018-01-23 16:43:22 +1100

    Moved things

---
+++ Discord RPC/DiscordRPC.Threaded/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiscordRPC.Threaded
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ DiscordRPC.NET3.5/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DiscordRPC.NET3._5
+{
+    public class Class1
+    {
+    }
+}
