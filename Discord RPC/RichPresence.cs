e5be4430a69e4aca771486d2b5215080995711ed
Lachee <lachlan.h@hotmail.com>
2018-01-24 15:59:55 +1100

    Cleaned up solution

--- Discord RPC/RichPresence.cs
+++ Discord RPC/RichPresence.cs
@@ -1,11 +1,5 @@
-﻿using DiscordRPC.Helper;
-using DiscordRPC.Serialization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DiscordRPC
 {
