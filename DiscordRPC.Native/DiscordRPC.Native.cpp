516adf355079e85b7d01c847255128bb3d78d2b2
Lachee <lachlan.h@hotmail.com>
2018-03-27 00:03:58 +1100

    Finished Connect

--- DiscordRPC.Native/DiscordRPC.Native.cpp
+++ DiscordRPC.Native/DiscordRPC.Native.cpp
@@ -2,14 +2,18 @@
 //
 
 #include "stdafx.h"
+#include <windows.h>
 #include "DiscordRPC.Native.h"
+
+HANDLE pipe;
+bool isOpen;
 
 extern "C" DISCORDRPCNATIVE_API bool isConnected()
 {
 	/*
 	bool connected = handle != nullptr && PipeConnected(handle);
 	*/
-	return false;
+	return pipe != INVALID_HANDLE_VALUE && isOpen;
 }
 
 extern "C" DISCORDRPCNATIVE_API bool readFrame(unsigned char* buffer, int length)
@@ -39,9 +43,37 @@ extern "C" DISCORDRPCNATIVE_API void close()
 
 extern "C" DISCORDRPCNATIVE_API bool open(char* pipename) 
 {
-	/*
-	handle = OpenFile(pipename);
-	*/
+	//Creates a connection to the pipe
+	pipe = ::CreateFileA(pipename, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+	if (pipe != INVALID_HANDLE_VALUE) 
+	{
+		isOpen = true;
+		return true;
+	}
+
+	//We have a error, better find out why.
+	auto lasterr = GetLastError();
+
+	//Pipe wasnt found
+	if (lasterr == ERROR_FILE_NOT_FOUND)
+		return false;
+
+	//Pipe is just busy
+	if (lasterr == ERROR_PIPE_BUSY) 
+	{
+		if (!WaitNamedPipeA(pipename, 10000))
+		{
+			//Failed to open, terminate
+			return false;
+		}
+		else 
+		{
+			//Attempt to open it again
+			return open(pipename);
+		}
+	}
+
+	//Some other magically error occured
 	return false;
 }
 
