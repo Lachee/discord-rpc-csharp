9c6d4d5c9975737f978bfca695eaeb125966e586
Almighty-Shogun <vschijndeljulian@gmail.com>
2022-03-06 19:21:43 +0100

    Moved `Converters` to `Core.Converters`

---
+++ DiscordRPC/Core/Converters/EnumValueAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DiscordRPC.Core.Converters
+{
+    internal class EnumValueAttribute : Attribute
+    {
+        public string Value { get; set; }
+        
+        public EnumValueAttribute(string value) => Value = value;
+    }
+}
