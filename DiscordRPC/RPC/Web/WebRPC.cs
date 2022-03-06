2b4c8c3e75366c7411c985a04103b73cac91805b
Almighty-Shogun <vschijndeljulian@gmail.com>
2022-03-06 19:28:48 +0100

    Seperated the classes for WebRPC

--- DiscordRPC/RPC/Web/WebRPC.cs
+++ DiscordRPC/RPC/Web/WebRPC.cs
@@ -1,14 +1,12 @@
-﻿using DiscordRPC.Exceptions;
-using DiscordRPC.RPC;
+﻿using System;
+using System.Net;
+using DiscordRPC.Core.Exceptions;
 using DiscordRPC.RPC.Commands;
 using DiscordRPC.RPC.Payload;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Net;
 
 #if INCLUDE_WEB_RPC
-namespace DiscordRPC.Web
+namespace DiscordRPC.RPC.Web
 {
 	/// <summary>
 	/// Handles HTTP Rich Presence Requests
@@ -151,40 +149,6 @@ namespace DiscordRPC.Web
 
 			string url = "http://127.0.0.1:" + port + "/rpc?v=" + RpcConnection.VERSION + "&client_id=" + applicationID;
 			return new WebRequest(url, json);
-		}
-	}
-
-	/// <summary>
-	/// Details of a HTTP Post request that will set the rich presence.
-	/// </summary>
-	[System.Obsolete("Web Requests is no longer supported as Discord removed HTTP Rich Presence support. See offical Rich Presence github for more information.")]
-	public struct WebRequest
-	{
-		private string _url;
-		private string _json;
-		private Dictionary<string, string> _headers;
-
-		/// <summary>
-		/// The URL to send the POST request too
-		/// </summary>
-		public string URL { get { return _url; } }
-
-		/// <summary>
-		/// The JSON formatted body to send with the POST request
-		/// </summary>
-		public string Data { get { return _json; } }
-
-		/// <summary>
-		/// The headers to send with the body
-		/// </summary>
-		public Dictionary<string, string> Headers { get { return _headers; } }
-
-		internal WebRequest(string url, string json)
-		{
-			_url = url;
-			_json = json;
-			_headers = new Dictionary<string, string>();
-			_headers.Add("content-type", "application/json");
 		}
 	}
 }
---
+++ DiscordRPC/RPC/Web/WebRequest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+#if INCLUDE_WEB_RPC
+namespace DiscordRPC.RPC.Web
+{
+    /// <summary>
+    /// Details of a HTTP Post request that will set the rich presence.
+    /// </summary>
+    [System.Obsolete("Web Requests is no longer supported as Discord removed HTTP Rich Presence support. See offical Rich Presence github for more information.")]
+    public struct WebRequest
+    {
+        private string _url;
+        private string _json;
+        private Dictionary<string, string> _headers;
+
+        /// <summary>
+        /// The URL to send the POST request too
+        /// </summary>
+        public string URL { get { return _url; } }
+
+        /// <summary>
+        /// The JSON formatted body to send with the POST request
+        /// </summary>
+        public string Data { get { return _json; } }
+
+        /// <summary>
+        /// The headers to send with the body
+        /// </summary>
+        public Dictionary<string, string> Headers { get { return _headers; } }
+
+        internal WebRequest(string url, string json)
+        {
+            _url = url;
+            _json = json;
+            _headers = new Dictionary<string, string>();
+            _headers.Add("content-type", "application/json");
+        }
+    }
+}
+#endif
